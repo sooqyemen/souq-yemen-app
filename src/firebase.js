8dfd282a2b974bcda2746fc2ce332d77eb23cf1e
sooqyemen <aboramez965@gmail.com>
2025-12-31 17:40:37 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,11 @@
-// ضع هنا إعدادات مشروع Firebase الخاص بك
-// من لوحة التحكم: Project settings -> General -> Your apps -> SDK setup and configuration
+// src/firebase.js
 
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 
+// الإعدادات اللي رسلتها لي:
 const firebaseConfig = {
   apiKey: "AIzaSyD_LRQdmb3Kyo6NVroUMvHGnx-Ciz9OIcU",
   authDomain: "aqarabhour-c8a9f.firebaseapp.com",
@@ -15,6 +16,12 @@ const firebaseConfig = {
   appId: "1:709287383516:web:008ccd7371f88c8c8f3f19"
 };
 
+// تشغيل Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+
+// الخدمات اللي نستخدمها في التطبيق
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export { app, auth, db, storage };
